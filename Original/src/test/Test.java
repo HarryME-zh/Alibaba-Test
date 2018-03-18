2900efe4b091685d01b14086da303c3ed19c6b80
Hongyi Zhang <honyiz@kth.se>
2018-03-18 20:36:40 +0100

    Test

---
+++ Original/src/test/Test.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import java.util.*;
+import java.math.*;
+/**
+ *
+ * @author harry
+ */
+public class Test {
+     static ArrayList<Integer> ARRAY_A = new ArrayList<Integer>();
+     static ArrayList<Integer> ARRAY_B = new ArrayList<Integer>();
+     static ArrayList<Integer> ARRAY_FINAL = new ArrayList<Integer>();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int finalResult = 0;
+        Random random = new Random();
+        System.out.println("Please input the length for the two arrary");
+        Scanner numInput = new Scanner(System.in);
+        int arrayLength = numInput.nextInt();
+        for(int i=0;i<arrayLength;i++){
+            int temp = random.nextInt(100);
+            ARRAY_A.add(temp);
+            temp = random.nextInt(100);
+            ARRAY_B.add(temp);
+        }
+        System.out.println("The array A is: "+ ARRAY_A);
+        System.out.println("The array B is: "+ ARRAY_B);
+        
+        for(int j = 0; j<arrayLength*2; j++)
+        {
+            boolean judgeFLG = random.nextBoolean();
+            if(judgeFLG){
+                if(ARRAY_A.size()!=0){
+                int temp = ARRAY_A.get(0);
+                ARRAY_FINAL.add(temp);
+                ARRAY_A.remove(0);}
+                else{
+                int temp = ARRAY_B.get(0);
+                ARRAY_FINAL.add(temp);
+                ARRAY_B.remove(0);
+                }
+            }else{
+                if(ARRAY_B.size()!=0){
+                int temp = ARRAY_B.get(0);
+                ARRAY_FINAL.add(temp);
+                ARRAY_B.remove(0);
+                }
+                else{
+                int temp = ARRAY_A.get(0);
+                ARRAY_FINAL.add(temp);
+                ARRAY_A.remove(0);
+                }
+            }
+        }
+            
+            for(int z=0; z<ARRAY_FINAL.size();){
+                finalResult= ARRAY_FINAL.get(z)*ARRAY_FINAL.get(z+1)+finalResult;
+                z = z+2;
+            }
+            System.out.println("The final array is: "+ ARRAY_FINAL);
+            System.out.println("The final result is: "+ finalResult);
+            
+        }
+       
+        
+    }
+    
+
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Alibaba-Test
+
+This is a little script to make two array mix themselves.
+
+Such as {a1,a2...ai,ai+1}, and {b1,b2...bi,bi+1} turns to {a1,a2,b1,a3,b2...ai+1,bi+1}
+
+The only principle is that for each array, a or b, their index behind should be larger than the front one in the final result array.
+And the output is the c1c2+c3c4+c5c6....
+
+Here is an example output
+
+Please input the length for the two arrary: 5
+
+The array A is: [40, 20, 25, 27, 31]
+
+The array B is: [36, 64, 84, 17, 62]
+
+The final array is: [36, 40, 64, 84, 17, 20, 25, 62, 27, 31]
+
+The final result is: 9543
+
+ 
