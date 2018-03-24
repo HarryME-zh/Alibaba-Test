9aa87421ea7a11919c74f41fc3eb599940bab631
Hongyi Zhang <honyiz@kth.se>
2018-03-24 09:10:02 +0100

    Final

--- Original/src/test/Test.java
+++ Original/src/test/Test.java
@@ -5,7 +5,6 @@
  */
 package test;
 import java.util.*;
-import java.math.*;
 /**
  *
  * @author harry
@@ -20,21 +19,51 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         int finalResult = 0;
-        Random random = new Random();
+       
         System.out.println("Please input the length for the two arrary");
         Scanner numInput = new Scanner(System.in);
         int arrayLength = numInput.nextInt();
+        
+        arrayGen(arrayLength);  //Generate two array
+        
+        System.out.println("The array A is: "+ ARRAY_A);
+        System.out.println("The array B is: "+ ARRAY_B);
+        
+        arrayCombine(arrayLength);  //Combine two array
+            
+        finalResult = resultCal();  //Calculate the final result
+        System.out.println("The final array is: "+ ARRAY_FINAL);
+        System.out.println("The final result is: "+ finalResult);
+            
+        }
+    
+    public static void arrayGen(int arrayLength){
+        Random random = new Random();
         for(int i=0;i<arrayLength;i++){
             int temp = random.nextInt(100);
             ARRAY_A.add(temp);
             temp = random.nextInt(100);
             ARRAY_B.add(temp);
         }
-        System.out.println("The array A is: "+ ARRAY_A);
-        System.out.println("The array B is: "+ ARRAY_B);
+    }
+    
+    
+    public static Integer resultCal(){
+        int finalResult = 0;
         
-        for(int j = 0; j<arrayLength*2; j++)
-        {
+         for(int z=0; z<ARRAY_FINAL.size();){
+                finalResult= ARRAY_FINAL.get(z)*ARRAY_FINAL.get(z+1)+finalResult;
+                z = z+2;
+            }
+        
+        
+        return finalResult;
+    }
+    
+    public static void arrayCombine(int arrayLength){        
+        Random random = new Random();
+         for(int j = 0; j<arrayLength*2; j++)
+         {
             boolean judgeFLG = random.nextBoolean();
             if(judgeFLG){
                 if(ARRAY_A.size()!=0){
@@ -59,15 +88,7 @@ public class Test {
                 }
             }
         }
-            
-            for(int z=0; z<ARRAY_FINAL.size();){
-                finalResult= ARRAY_FINAL.get(z)*ARRAY_FINAL.get(z+1)+finalResult;
-                z = z+2;
-            }
-            System.out.println("The final array is: "+ ARRAY_FINAL);
-            System.out.println("The final result is: "+ finalResult);
-            
-        }
+    }
        
         
     }
